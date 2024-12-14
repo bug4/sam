b2f9ab5b4041f6ec203b6f15a313c2f939eb4d00
bug4 <bugad22@gmail.com>
2024-12-14 04:44:46 +0200

    Before eject

--- src/App.js
+++ src/App.js
@@ -416,7 +416,7 @@ export default function App() {
       <nav className="fixed top-0 w-full z-40 flex justify-center items-center p-4">
   <div className="flex gap-6">
   <a
-      href="#twitter"
+      href="https://x.com/"
       onClick={playClick}
       target="_blank"
       rel="noopener noreferrer"
@@ -433,7 +433,7 @@ export default function App() {
     </a>
 
     <a
-      href="#telegram"
+      href="https://telegram.org/"
       onClick={playClick}
       target="_blank"
       rel="noopener noreferrer"
@@ -450,7 +450,7 @@ export default function App() {
     </a>
 
     <a
-      href="#"
+      href="#!"
       onClick={(e) => {
         e.preventDefault();
         playClick();
