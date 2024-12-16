cde1f36a95e740193bf162a77ec47cd626fffc16
bug4 <bugad22@gmail.com>
2024-12-16 18:38:28 +0200

    Ready for deployment

--- src/App.js
+++ src/App.js
@@ -527,7 +527,7 @@ export default function App() {
       <nav className="fixed top-0 w-full z-40 flex justify-center items-center p-4">
   <div className="flex gap-6">
   <a
-      href="https://x.com/"
+      href="https://x.com/AeonProtocolAI"
       onClick={playClick}
       target="_blank"
       rel="noopener noreferrer"
@@ -544,7 +544,7 @@ export default function App() {
     </a>
 
     <a
-      href="https://t.me/"
+      href="https://t.me/aeonprotocol"
       onClick={playClick}
       target="_blank"
       rel="noopener noreferrer"
